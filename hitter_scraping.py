fe938fa8c30f06ba5fccc82bcea3ad2810e97c3a
Patrick Bovard <pbovard63@gmail.com>
2021-06-16 09:59:31 -0500

    Updating scraping files

---
+++ hitter_scraping.py
@@ -0,0 +1,14 @@
+#Importing needed packages:
+import pandas as pd
+import matplotlib.pyplot as plt
+from pylab import rcParams
+%matplotlib inline
+rcParams['figure.figsize'] = 20,10
+import numpy as np
+import glob
+from scipy import stats
+from bs4 import BeautifulSoup
+import requests
+import re
+from IPython.core.display import display, HTML
+
